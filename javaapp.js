55d887e5ccfc97bb7d719acfdb9197e4739dfe62
maariahcaro <93722341+maariahcaro@users.noreply.github.com>
2021-11-07 12:19:35 +0100

    fd

--- javaapp.js
+++ javaapp.js
@@ -51,6 +51,7 @@ let now = new Date(timestamp);
         let humidityElement = document.querySelector("#humidity");
         let windElement = document.querySelector("#wind");
         let iconElement = document.querySelector("#icon");
+        let descriptionElement = document.querySelector("#description");
         temperatureElement.innerHTML = response.data.main.temp;
         cityElement.innerHTML = response.data.name;
         humidityElement.innerHTML = response.data.main.humidity;
@@ -59,14 +60,16 @@ let now = new Date(timestamp);
         let dateElement = document.querySelector("#date");
         dateElement.innerHTML = formatDate(response.data.dt * 1000);
         iconElement.setAttribute("src") = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+        descriptionElement.innerHTML = response.data.weather[0].description;
     
       }
+      function search(city) {
 
-      let key ="50f08580ddb58d03ac1e0e37f19dd297";
-      let city = "Sydney";
+let key ="50f08580ddb58d03ac1e0e37f19dd297";
+      
 let url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
 
-axios.get(`${url}&appid=${key}`).then(displayTemperature);
+axios.get(url).then(displayTemperature); }
 
 function showTemperature(response) {
     console.log(response.data); 
