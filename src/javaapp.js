20d3fd4fc832cefec3647c557633ba74d6facf0c
maariahcaro <93722341+maariahcaro@users.noreply.github.com>
2021-11-07 11:49:01 +0100

    ds

--- src/javaapp.js
+++ src/javaapp.js
@@ -63,7 +63,7 @@ let now = new Date(timestamp);
       }
 
       let key ="50f08580ddb58d03ac1e0e37f19dd297";
-      let city = "Paris";
+      let city = "Sydney";
 let url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
 
 axios.get(`${url}&appid=${key}`).then(displayTemperature);
