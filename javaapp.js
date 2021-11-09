0f99d3cd3cc38de15494cecb33ae56ed64d0197a
maariahcaro <93722341+maariahcaro@users.noreply.github.com>
2021-11-09 18:05:28 +0100

    bkj

--- javaapp.js
+++ javaapp.js
@@ -72,7 +72,7 @@ function getForecast(coordinates) {
       
 let url =`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${key}&units=metric`;
 
-axios.get(apiUrl).then(displayForecast);
+axios.get(url).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -104,7 +104,7 @@ function displayTemperature(response) {
 function search(city) {
   let key = "50f08580ddb58d03ac1e0e37f19dd297";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
+  axios.get(url).then(displayTemperature);
 }
 
 function handleSubmit(event) {
