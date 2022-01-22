00c607a411eb7bf34bf11ac5d3ae15b355b89f81
maariahcaro <93722341+maariahcaro@users.noreply.github.com>
2022-01-22 20:08:29 +0100

    Update javaapp.js

--- javaapp.js
+++ javaapp.js
@@ -22,51 +22,6 @@ function formatDate(date) {
 
   return `${day} ${hours}:${minutes}`;
 }
-function formatDay(time) {
-  let date = new Date(time * 1000);
-  let day = date.getDay();
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-
-  return days[day];
-
-}
-
-function displayForecast(response) {
-  let forecast = response.data.daily;
-  let forecastElement = document.querySelector("#forecast");
-
-  let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay, index) {
-    if (index < 4) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
-        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>${index}
-        <img
-          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
-          alt=""
-          width="30"
-        />
-        <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${forecastDay.temp.max}° </span>
-          <span class="weather-forecast-temperature-min"> ${forecastDay.temp.min}° </span>
-        </div>
-      </div>
-  `;
-}
-  });
-
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
-}
-
-function displayForecast(coordinates); {
-  let api = "50f08580ddb58d03ac1e0e37f19dd297";
-  let apiurl = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}=${coordinates.lat}{lon}=${coordinates.lon}&appid=${api}&unit=metric`;
-axios.get(apiurl).then(displayForecast);
-
-  displayForecast(response.data.coord);
-}
 
 function displayWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
@@ -77,17 +32,9 @@ function displayWeather(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
-  celsiusTemperature = response.data.main.temp;
 
   document.querySelector("#opis").innerHTML = response.data.weather[0].main;
-  let opisElement = document.querySelector("#opis");
-  opisElement.innerHTML = response.data.weather[0].description;
-  let iconElement = document.querySelector("icon");
-  iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` );
-  iconElement.setAttribute("alt" `response.data.weather[0].description`);
-
 }
-
 
 function searchCity(city) {
   let api = "50f08580ddb58d03ac1e0e37f19dd297";
@@ -112,25 +59,6 @@ function showCurrentLocation(event) {
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
-}
-
-function displayCelsius(event){
-event.preventDefault();
-celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-let temperatureElement = document.querySelector("#temperature");
-temperatureElement.innerHTML = Math.round(celsiusTemp);
-}
-let celsiusTemp = null;
-
-
 let h6 = document.querySelector("h6");
 let currentTime = new Date();
 h6.innerHTML = formatDate(currentTime);
@@ -138,16 +66,14 @@ h6.innerHTML = formatDate(currentTime);
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
-let fahrenheitLink = document.querySelector("#fahrenheit");
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
-let celsiusLink = document.querySelector("#celsius");
-celsiusLink.addEventListener("click", displayCelsius);
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
 
 let currentloc = document.querySelector("#current");
 currentloc.addEventListener("click", showCurrentLocation);
 
-
-iconElement
-
-searchCity("New York");
+search("New York");
+displayForecast();
